6924a67c9f7a48b2579008e1fe9df118d014c2c6
szt2palpat <szt2palpat@vasvari.org>
2023-09-14 13:04:23 +0200

    Basicinfo

---
+++ src/Basicinfo.java
@@ -0,0 +1,10 @@
+public class Basicinfo {
+    public static void main(String[] args) {
+        String name="Pálvölgyi Patrik";
+        int age=19;
+        double hieght=173.5;
+        boolean married=false;
+        System.out.println(name+" "+age+" "+hieght+" "+married);
+
+    }
+}
